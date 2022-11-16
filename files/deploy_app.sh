000759c17a95d4ca3fc65b29301cb64a716519ab
Juan Jimenez <Juangg81@users.noreply.github.com>
2022-11-16 15:36:56 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Este es un Gato desde Terraform.
+  Welcome to ${PREFIX}'s app. Este es un Gato que ACME te regalará por la compra de las patinetas con jet
   <!-- END -->
 
   </div>
